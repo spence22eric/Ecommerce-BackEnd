105fd1036b950c9f8c348f6c5abaef9e62ca9e0c
Eric Spencer <spencer22eric@gmail.com>
2022-06-08 19:56:01 -0400

    edit models, associations progress

--- models/index.js
+++ models/index.js
@@ -5,10 +5,18 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag)
 
 // Tags belongToMany Products (through ProductTag)
 
