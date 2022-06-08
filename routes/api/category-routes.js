391999f82e857a5b6b4327d5154982515f6c87cd
Eric Spencer <spencer22eric@gmail.com>
2022-06-08 19:41:25 -0400

    add get all and get one category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,12 +5,26 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  Category.findAll()
+  .then(data => {
+    res.json(data);
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(data => {
+    res.json(data);
+  })
+  .catch(err => res.status(500).json(err));
 });
 
 router.post('/', (req, res) => {
