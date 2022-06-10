9ef9b36476a64a3d6632abf1a0dfc07e3d58c523
Eric Spencer <spencer22eric@gmail.com>
2022-06-09 23:03:48 -0400

    add delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -114,6 +114,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(data => res.json(data))
+  .catch(err => res.status(400).json(err))
 });
 
 module.exports = router;
