8e4eaa692e9f9762c2d2d8ef982194e3a13dc649
Eric Spencer <spencer22eric@gmail.com>
2022-06-09 23:10:40 -0400

    add get one tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -12,7 +12,13 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(data => res.json(data))
+  .catch(err => res.status(400).json(err))
 });
 
 router.post('/', (req, res) => {
