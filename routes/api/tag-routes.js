4ed7999b89f8152aabe08f76e2e2c7e3c02eace6
Eric Spencer <spencer22eric@gmail.com>
2022-06-09 23:08:50 -0400

    add get all tags route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,7 +5,9 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll()
+  .then(data => res.json(data))
+  .catch(err => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
