c6cc5c99ed389241411b38f48891fb11309fd180
Eric Spencer <spencer22eric@gmail.com>
2022-06-10 00:18:42 -0400

    add post tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -23,6 +23,9 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then(newData => res.json(newData))
+  .catch(err => res.status(400).json(err))
 });
 
 router.put('/:id', (req, res) => {
